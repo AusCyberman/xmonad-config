87070c67d942acef917f00998a4d83d22dda3840
AusCyber <willp@outlook.com.au>
2021-07-31 15:03:08 +1000

    initialised repo

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "my xmonad configuration";
+  inputs = {
+    xmonad = {
+      url = "github:auscyberman/xmonad";
+      flake = false;
+    };
+    xmonad-contrib = {
+      #            url = "github:auscyberman/xmonad-contrib";
+      #url = "/home/auscyber/xmonad-contrib";
+      url = "github:xmonad/xmonad-contrib";
+      flake = false;
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+  outputs = { nixpkgs, xmonad, xmonad-contrib, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        haskellOverlay = final: prev: {
+          haskellPackages = prev.haskellPackages.override {
+            overrides = self: super: {
+              #ghc = prev.haskell.compiler.ghc901;
+#              X11 = self.X11_1_10;
+              xmonad = self.callCabal2nix "xmonad" xmonad { };
+              xmonad-contrib =
+                self.callCabal2nix "xmonad-contrib" xmonad-contrib { };
+              my-xmonad = self.callCabal2nix "my-xmonad" (./.) { };
+
+            };
+          };
+
+        };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ haskellOverlay ];
+        };
+      in
+      {
+        defaultPackage = pkgs.haskellPackages.my-xmonad;
+        packages = {
+          inherit (pkgs.haskellPackages) my-xmonad xmonad xmonad-contrib;
+        };
+        defaultOverlay = haskellOverlay;
+
+      });
+}
